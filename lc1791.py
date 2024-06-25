09453469df1134ecba20d344c3c8e3f2113c1ec2
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-06-25 17:17:20 +0100

    optimal sol

--- lc1791.py
+++ lc1791.py
@@ -1,10 +1,5 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        edge_dict = defaultdict(int)
-        for e in edges:
-            edge_dict[e[0]] += 1
-            edge_dict[e[1]] += 1
-        
-        for k, v in edge_dict.items():
-            if v > 1:
-                return k
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        return edges[0][1]
