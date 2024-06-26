9287ec4a1333a1636655d22747d5fa0ba25ca209
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-06-26 16:41:24 +0100

    min vertices to reach all nodes

---
+++ lc1557.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        ans = []
+        indegree = defaultdict(int)
+        outdegree = defaultdict(int)
+        for source, dest in edges:
+            indegree[dest] += 1
+            outdegree[source] += 1
+        for i in range(n):
+            if indegree[i] == 0 and outdegree[i] > 0:
+                ans.append(i)
+        return ans
