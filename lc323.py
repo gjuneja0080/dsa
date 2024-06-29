aa5f2ced3147824514278c733ca4915b63c587b3
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-06-29 14:24:45 +0100

    connected comp in undir graph dfs+bfs

--- lc323.py
+++ lc323.py
@@ -18,3 +18,32 @@ class Solution:
                 dfs(i)
                 comp_count += 1
         return comp_count
+
+
+##############################################################################
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        comp_count = 0
+        visited = [False] * n
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def bfs(current_node):
+            nonlocal comp_count
+            queue = deque([current_node])
+            visited[current_node] = True
+            while queue:
+                current_node = queue.popleft()
+                for next_node in graph[current_node]:
+                    if not visited[next_node]:
+                        visited[next_node] = True
+                        queue.append(next_node)
+
+        for i in range(n):
+            if not visited[i]:
+                comp_count += 1
+                bfs(i)
+
+        return comp_count
