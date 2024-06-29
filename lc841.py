4d8ff5f1d688ee0b0fb542c4bc97d4fe7f6b6453
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-06-29 11:33:43 +0100

    keys&rooms - dis + bfs

--- lc841.py
+++ lc841.py
@@ -13,3 +13,19 @@ class Solution:
                     queue.append(next_room)
 
         return False if False in visited else True
+######################################################################
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        visited = [False] * len(rooms)
+        visited[0] = True
+
+        def dfs(current_room):
+            for next_room in rooms[current_room]:
+                if not visited[next_room]:
+                    visited[next_room] = True
+                    dfs(next_room)
+        dfs(0)
+
+        return False if False in visited else True
