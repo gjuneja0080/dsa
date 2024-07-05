12026f77796829768c9c299079abb79d0bd662b3
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-07-05 23:31:31 +0100

    surrounded regions

--- lc130.py
+++ lc130.py
@@ -36,3 +36,39 @@ class Solution:
             if isValid:
                 for row, col in region_dict[region]:
                     board[row][col] = 'X'
+##################################################################################################################
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols = len(board), len(board[0])
+        def dfs(row, col):
+            if row < 0 or col < 0 \
+                or row >= rows or col >= cols \
+                or board[row][col] != 'O':
+                return
+
+            board[row][col] = 'T'
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O' \
+                    and row in (0, rows - 1) \
+                    or col in (0, cols - 1):
+                    dfs(row, col)
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'T':
+                    board[row][col] = 'O'
