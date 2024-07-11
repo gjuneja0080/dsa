6f9df97eb0ba363ccf1827d148e5d494a23a2520
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-07-12 00:36:41 +0100

    find bottom leftmost val
    bfs+dfs

--- lc513.py
+++ lc513.py
@@ -19,3 +19,28 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return leftmost_val
+#################################################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        level_dict = defaultdict(list)
+        def dfs(node, level):
+            if not node: return
+            if node.left:
+                dfs(node.left, level + 1)
+            if node.right:
+                dfs(node.right, level + 1)
+            level_dict[level].append(node.val)
+        dfs(root, 1)
+        return level_dict[max(level_dict.keys())][0]
+            
+
+
+
+            
+            
