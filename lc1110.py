4eda0e19b1a857af8bc47de2161bf6ead780afd6
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-07-17 23:37:18 +0100

    delete nodes and return forest

--- lc1110.py
+++ lc1110.py
@@ -6,21 +6,22 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+
         def dfs(node, previous, direction):
-            if not node: return None
+            if not node: return
             if node.left: dfs(node.left, node, 'left')
             if node.right: dfs(node.right, node, 'right')
-            
-            if (node.val in to_delete) and (node.val not in visited):
-                visited.add(node.val)
+
+            if node.val in to_delete:
                 if node.left: res.append(node.left)
                 if node.right: res.append(node.right)
                 if direction == 'left':
                     previous.left = None
                 elif direction == 'right':
-                    previous.right = None 
+                    previous.right = None
+
         res = []
-        visited = set()
+        to_delete = set(to_delete)
         dfs(root, None, '')
         if root.val not in to_delete:
             res.append(root)
