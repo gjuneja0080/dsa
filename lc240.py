69b3fccab5df6b53bcb57b89b100318614e69a83
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-08-10 18:13:35 +0100

    search a 2d matrix II
    search space reduction approach

--- lc240.py
+++ lc240.py
@@ -16,3 +16,17 @@ class Solution:
             if binary_search(row):
                 return True
         return False
+############################################################################################################
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        up, rp = rows - 1, 0
+        while up > -1 and rp < cols:
+            if target > matrix[up][rp]:
+                rp += 1
+            elif target < matrix[up][rp]:
+                up -= 1
+            else:
+                return True
+        return False
