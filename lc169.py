dcc5bc62ca8d08af84fdeb687ef870cd6afa8cfb
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2024-08-20 22:31:32 +0100

    Update lc169.py - Boyer Moore voting algo

--- lc169.py
+++ lc169.py
@@ -13,3 +13,15 @@ class Solution:
             else:
                 count += 1
         return cur_element
+#############################################################################
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count, candidate = 0, 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num != candidate:
+                count -= 1
+            else:
+                count += 1
+        return candidate
