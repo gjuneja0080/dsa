39daca3cca26d6e8767bb700c81eb1a3c4ed5dd1
gjuneja0080 <58052453+gjuneja0080@users.noreply.github.com>
2025-02-13 02:54:11 +0000

    leetflex banned accounts

---
+++ lc1747.sql
@@ -0,0 +1,6 @@
+select distinct l1.account_id
+from loginfo l1 join loginfo l2
+on l1.account_id = l2.account_id
+and l1.ip_address <> l2.ip_address 
+and (l1.login between l2.login and l2.logout
+or l2.login between l1.login and l1.logout)
